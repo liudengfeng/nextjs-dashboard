f41c43775189d938823443b8065de7f4ec9695e1
liudengfeng <liudengfeng_sd@outlook.com>
2024-08-05 17:09:29 +0800

    refactor: Add authentication action to login form

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { authenticate } from "@/app/lib/actions";
 import { lusitana } from "@/app/ui/fonts";
 import {
   AtSymbolIcon,
@@ -9,13 +10,13 @@ import {
 import { ArrowRightIcon } from "@heroicons/react/20/solid";
 import { Button } from "./button";
 import { useActionState } from "react";
-import { authenticate } from "@/app/lib/actions";
 
 export default function LoginForm() {
   const [errorMessage, formAction, isPending] = useActionState(
     authenticate,
     undefined
   );
+
   return (
     <form action={formAction} className="space-y-3">
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
